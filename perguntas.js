68b5d2c04d4bd4c55878f9d8d4e8fa19f016731e
gabigiordano10 <gabigiordano10@gmail.com>
2024-11-27 08:50:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Quem foi Pedro Álvares Cabral?',
+    ' foi um navegador e explorador português responsável por encontrar, em 1500, as terras pertencentes ao Brasil.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual e a atual capital da Itália?',
+    'A capital da Italia é Roma'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'qual obra mais famosa de machado de assis?',
+    'Memórias Póstumas de Brás Cubas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
